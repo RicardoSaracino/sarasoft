7046998b5c02a81e7fda9069f8c7a0a25b53c0b0
Ricardo Saracino <ricardo.saracino@ricardosaracino.com>
2016-10-18 19:42:16 -0400

    FullCalendar setup https://packagist.org/packages/ancarebeca/full-calendar-bundle#v3.0.1 https://fullcalendar.io/

---
+++ src/AppBundle/Event/CalendarEvent.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Ricardo Saracino
+ * @since 10/18/16
+ */
+
+namespace AppBundle\Event;
+
+use AncaRebeca\FullCalendarBundle\Model\Event as BaseEvent;
+
+class CalendarEvent extends BaseEvent {
+
+}
