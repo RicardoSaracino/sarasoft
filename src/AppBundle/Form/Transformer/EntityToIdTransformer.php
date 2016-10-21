32868a4792a3ea16d6a2ef2db9b84af91e74238e
Ricardo Saracino <ricardo.saracino@ricardosaracino.com>
2016-10-20 23:43:14 -0400

    hidden EntityHiddenType

---
+++ src/AppBundle/Form/Transformer/EntityToIdTransformer.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @author Ricardo Saracino
+ * @since 10/20/16
+ */
+
+namespace AppBundle\Form\Transformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\InvalidConfigurationException;
+use Symfony\Component\Form\Exception\TransformationFailedException;;
+
+/**
+ * Class EntityToIdTransformer
+ * @package AppBundle\Form\Transformer
+ */
+class EntityToIdTransformer implements DataTransformerInterface
+{
+	/**
+	 * @param mixed $entity
+	 * @return mixed|null
+	 * @throws \Symfony\Component\Form\Exception\InvalidConfigurationException
+	 */
+	public function transform($entity)
+	{
+		#dump($entity);    die;
+
+		if (null === $entity) {
+			return null;
+		}
+
+		if (!method_exists($entity, 'getId')) {
+			throw new InvalidConfigurationException(sprintf('There is no method getId for class "%s".', get_class($entity)));
+		}
+
+		return $entity->getId();
+	}
+
+	/**
+	 * @param mixed $id
+	 * @return mixed|null|object
+	 * @throws \Symfony\Component\Form\Exception\TransformationFailedException
+	 */
+	public function reverseTransform($id)
+	{
+		if (!$id) {
+			return null;
+		}
+
+		$entity = $this->objectManager
+			->getRepository($this->class)
+			->find($id);
+
+		if (null === $entity) {
+			throw new TransformationFailedException();
+		}
+
+		return $entity;
+	}
+}
