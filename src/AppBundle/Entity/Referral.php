d633280e71b0752071d35f44ad41d5d0e6fbcd66
Ricardo Saracino <ricardo.saracino@ricardosaracino.com>
2016-10-26 21:50:24 -0400

    added referral

---
+++ src/AppBundle/Entity/Referral.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ *
+ * Referral
+ *
+ * @ORM\Table(name="referral", uniqueConstraints={@ORM\UniqueConstraint(name="name", columns={"name"})}, indexes={@ORM\Index(name="created_by", columns={"created_by"}), @ORM\Index(name="updated_by", columns={"updated_by"})})
+ * @ORM\Entity
+ *
+ * @UniqueEntity("name")
+ */
+class Referral
+{
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="id", type="integer", nullable=false)
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="IDENTITY")
+	 */
+	private $id;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="name", type="string", length=32, nullable=false)
+	 *
+	 * @Assert\NotBlank()
+	 * @Assert\Length(min=3)
+	 */
+	private $name;
+
+	/**
+	 * @var \DateTime
+	 *
+	 * @ORM\Column(name="created_at", type="datetime", nullable=false)
+	 *
+	 * @Gedmo\Timestampable(on="create")
+	 */
+	private $createdAt;
+
+
+	/**
+	 * @var \AppBundle\Entity\User
+	 *
+	 * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+	 * @ORM\JoinColumns({
+	 * @ORM\JoinColumn(name="created_by", referencedColumnName="id")
+	 * })
+	 *
+	 * @Gedmo\Blameable(on="create")
+	 */
+	private $createdBy;
+
+	/**
+	 * @var \DateTime
+	 *
+	 * @ORM\Column(name="updated_at", type="datetime", nullable=false)
+	 *
+	 * @Gedmo\Timestampable(on="update")
+	 */
+	private $updatedAt;
+
+	/**
+	 * @var \AppBundle\Entity\User
+	 *
+	 * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+	 * @ORM\JoinColumns({
+	 * @ORM\JoinColumn(name="updated_by", referencedColumnName="id")
+	 * })
+	 *
+	 * @Gedmo\Blameable(on="update")
+	 */
+	private $updatedBy;
+
+	/**
+	 * @return string
+	 */
+	public function __toString()
+	{
+		return $this->name;
+	}
+
+	/**
+	 * Get id
+	 *
+	 * @return integer
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * Set name
+	 *
+	 * @param integer $name
+	 *
+	 * @return Referral
+	 */
+	public function setName($name)
+	{
+		$this->name = $name;
+
+		return $this;
+	}
+
+	/**
+	 * Get name
+	 *
+	 * @return integer
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	/**
+	 * Get createdAt
+	 *
+	 * @return \DateTime
+	 */
+	public function getCreatedAt()
+	{
+		return $this->createdAt;
+	}
+
+	/**
+	 * Get createdBy
+	 *
+	 * @return \AppBundle\Entity\User
+	 */
+	public function getCreatedBy()
+	{
+		return $this->createdBy;
+	}
+
+	/**
+	 * Get updatedAt
+	 *
+	 * @return \DateTime
+	 */
+	public function getUpdatedAt()
+	{
+		return $this->updatedAt;
+	}
+
+	/**
+	 * Get updatedBy
+	 *
+	 * @return \AppBundle\Entity\User
+	 */
+	public function getUpdatedBy()
+	{
+		return $this->updatedBy;
+	}
+}
