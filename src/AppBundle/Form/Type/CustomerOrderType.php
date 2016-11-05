58f8acebdf4062ffe368d0a79e37e32856665ef0
Ricardo Saracino <ricardo.saracino@ricardosaracino.com>
2016-11-05 00:09:44 -0400

    code formatting

--- src/AppBundle/Form/Type/CustomerOrderType.php
+++ src/AppBundle/Form/Type/CustomerOrderType.php
@@ -19,44 +19,39 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 class CustomerOrderType extends AbstractType
 {
 	/**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
+	 * {@inheritdoc}
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
 			->add('orderStatusCode', Type\TextType::class, ['label' => 'Order Status', 'disabled' => true])
 			->add('company', EntityType::class, ['class' => Company::class, 'choice_label' => 'name', 'placeholder' => 'Choose'])
 			->add('referral', EntityType::class, ['class' => Referral::class, 'choice_label' => 'name', 'placeholder' => 'Choose'])
 			->add('bookedFrom', MyType\UserDateTimePickerType::class, ['label' => 'Booked From'])
 			->add('bookedUntil', MyType\UserDateTimePickerType::class, ['label' => 'Booked Until']);
 
-
-		if($builder->getData()->getBookingNotes())
-		{
-			$builder->add('bookingNotes', MyType\UserTextAreaPrependType::class, ['label' => 'Booking Notes', 'trim' => true, 'data' => $builder->getData(), 'data_class' => CustomerOrder::class]);
-		}
-		else
-		{
+		if ($builder->getData()->getBookingNotes()) {
+			$builder->add('bookingNotes', MyType\UserTextAreaPrependType::class, [
+				'label' => 'Booking Notes', 'trim' => true, 'data' => $builder->getData(), 'data_class' => CustomerOrder::class]);
+		} else {
 			$builder->add('bookingNotes', MyType\UserTextAreaType::class, ['label' => 'Booking Notes', 'trim' => true]);
 		}
-
 
         $builder->add('customerOrderServices', Type\CollectionType::class, [
 			'entry_type' => CustomerOrderServiceType::class,
 			'label' => 'Services',
 	        'allow_add' => true,
 			'allow_delete' => true,
-        ]);
-
+		]);
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-        	'data_class' => CustomerOrder::class
+	/**
+	 * {@inheritdoc}
+	 */
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults([
+			'data_class' => CustomerOrder:class
         ]);
     }
 }
