fc103627e9bb0dbb2eb6de41cc796644c45e9c6e
Ricardo Saracino <ricardo.saracino@ricardosaracino.com>
2017-02-11 12:48:31 -0500

    formatting

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,8 +2,8 @@
 
 namespace AppBundle\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
 use AppBundle\Validator\Constraints as AppAssert;
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Intl\Intl;
